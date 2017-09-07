b853aa32f444d7f0a278034193dd6bf3cbf61581
Amanda Greenberry <green6erry@gmail.com>
2017-09-07 17:48:31 +0100

    added components

--- js/index.js
+++ js/index.js
@@ -1,2 +1,12 @@
+// js/index.js
 require('babel-polyfill');
 
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var PersonList = require('./components/person-list');
+
+document.addEventListener('DOMContentLoaded', function() {
+    ReactDOM.render(<PersonList />, document.getElementById('app'));
+});
